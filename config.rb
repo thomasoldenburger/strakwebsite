8501a6949444fbae83acf4bae18eec4159c79f70
Emile Bosch <emilebosch@me.com>
2016-08-31 12:35:33 +0200

    Fix urls

--- config.rb
+++ config.rb
@@ -24,6 +24,11 @@
 #   page "/admin/*"
 # end
 
+
+# data.producten.each do |index, product|
+#   proxy "/about/#{product.name}.html", "producten/learning-groups.html", :locals => { :product => product }
+# end
+
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
@@ -58,13 +63,13 @@ activate :autoprefixer
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
